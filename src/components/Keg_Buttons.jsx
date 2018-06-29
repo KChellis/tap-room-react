ff3a1d54847c343b526829888a6d7760d4bfabed
Kristen <peanutster@gmail.com>
2018-06-29 14:52:06 -0700

    create and add content to Buttons Component

---
+++ src/components/Keg_Buttons.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Buttons(){
+
+  return(
+    <div className='buttons'>
+      <button>Edit</button>
+      <button>Delete</button>
+      <br/>
+      <button>Pint</button>
+      <button>Growler(32 oz)</button>
+      <button>Growler(64 oz)</button>
+    </div>
+  );
+}
+
+export default Buttons;
